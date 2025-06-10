85268b9d9b42b2d67776f882b3ae08cac9aa574d
Avneet kaur <avneetbagga06@gmail.com>
2025-06-10 23:02:30 +0530

    url

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['http://localhost:5173', 'https://mern-blogs-ten.vercel.app'];
 app.use(cors({ origin : allowedOrigins, credentials : true}));
 
 
